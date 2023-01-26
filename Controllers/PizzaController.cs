2168e06386fb5aa9c06298ba31f3582dc8e2de06
Saqib Amin <saqibamin257@gmail.com>
2023-01-26 16:53:40 +0000

    added PizzaController

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+    public PizzaController()
+    {
+
+    }
+
+    //GET all action
+
+    //GET by Id action
+
+    //POST action
+
+    //PUSH action
+
+    //DELETE action
+
+}
